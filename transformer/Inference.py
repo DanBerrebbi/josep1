1a93898bff0c71b53014dd216e8a50fb0fc1158e
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-05-14 18:36:10 +0200

    pb2

--- transformer/Inference.py
+++ transformer/Inference.py
@@ -64,7 +64,7 @@ class Inference():
 
         if self.model_type == 'sxsxs_sc':
           src, self.msk_src = prepare_source(batch_idxs[0], self.src_voc.idx_pad, self.device) #src is [bs, ls] msk_src is [bs,1,ls]
-          xsrc, msk_xsrc = prepare_source(batch_idxs[1], self.src_voc.idx_pad, self.device) #xsrc is [bs, lxs] msk_xsrc is [bs,1,lxs]
+          xsrc, self.msk_xsrc = prepare_source(batch_idxs[1], self.src_voc.idx_pad, self.device) #xsrc is [bs, lxs] msk_xsrc is [bs,1,lxs]
           xtgt, self.msk_xtgt = prepare_source(batch_idxs[2], self.tgt_voc.idx_pad, self.device) #xtgt is [bs, lxt] msk_xtgt is [bs,1,lxt]
           self.z_src, self.z_xsrc, self.z_xtgt = self.model.encode(src, xsrc, xtgt, self.msk_src, msk_xsrc, self.msk_xtgt) #[bs,ls,ed] [bs,lxt,ed]
 
